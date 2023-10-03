dc6cd2085520fde883f071b3a38f8db75e23ab85
Nahor Nehc <90894291+Nahor-Nehc@users.noreply.github.com>
2023-10-03 21:20:46 +0100

    new textures

--- components/textures.py
+++ components/textures.py
@@ -3,33 +3,23 @@ from pygame import Rect, image
 SIZE = 40
 
 TEXTURE_DICTIONARY = {
-  "wall":(0, 0),
-  "wall-top-left":(SIZE, 0),
-  "wall-left":(SIZE*2, 0),
-  "wall-bottom-left":(SIZE*3, 0),
-  "wall-bottom":(SIZE*4, 0),
-  "wall-bottom-right":(SIZE*5, 0),
-  "wall-right":(SIZE*6, 0),
-  "wall-top-right":(SIZE*7, 0),
-  "wall-top":(SIZE*8, 0),
-  "wall-middle":(SIZE*9, 0),
+  "black":(0, 0),
+  "dark":(SIZE, 0),
+  "medium":(SIZE*2, 0),
+  "light":(SIZE*3, 0),
+  "white":(SIZE*4, 0),
   "empty":(0, SIZE),
 }
 
-# properties: ["collide/no collide", ]
+# properties: ["letter representation"]
 wall_properties = [1]
 
 TEXTURE_PROPERTIES = {
-  "wall":wall_properties,
-  "wall-top-left":wall_properties,
-  "wall-left":wall_properties,
-  "wall-bottom-left":wall_properties,
-  "wall-bottom":wall_properties,
-  "wall-bottom-right":wall_properties,
-  "wall-right":wall_properties,
-  "wall-top-right":wall_properties,
-  "wall-top":wall_properties,
-  "wall-middle":wall_properties,
+  "black":["b"],
+  "dark":["d"],
+  "medium":["m"],
+  "light":["l"],
+  "white":["w"],
   "empty":[],
 }
 
